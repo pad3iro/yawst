7a6d211d7316bac29a6b9ae64db5c0b29c0022c8
pad3iro <pad3iro@mail.com>
2023-11-04 10:36:22 +0000

    refactoring whatweb_to_html.py

--- resources/utils/whatweb_to_html.py
+++ resources/utils/whatweb_to_html.py
@@ -4,6 +4,9 @@ def create_html_report(jsonFile):
 
     jsonInput = json.load(open(jsonFile))
 
+    IDENT_4 = '\t\t\t\t'
+    IDENT_5 = '\t\t\t\t\t'
+
     html = """<html>
     <head>
         <title> Whatweb Report</title>
@@ -23,27 +26,29 @@ def create_html_report(jsonFile):
 
         request_config = element["request_config"]
         if not request_config_processed and len(request_config) > 0:
-            html += '\t\t\t\t<table style="width:100%; background-color: lightsteelblue; padding-left: 20px; border: 1px solid black; border-radius: 10px; border-collapse: separate; border-spacing: 1em;">\n'
-            html += '\t\t\t\t\t<tr><td><h3>Request Config</h4></td></tr>\n'
+            html += IDENT_4 + '<table style="width:100%; background-color: lightsteelblue; padding-left: 20px; border: 1px solid black; border-radius: 10px; border-collapse: separate; border-spacing: 1em;">\n'
+            html += IDENT_5 + '<tr><td><h3>Request Config</h4></td></tr>\n'
             for config in request_config:
                 first=True
                 for config_key in request_config[config]:
                     value = "%s:%s"%(config_key,request_config[config][config_key]) 
                     if first:
-                        html += '\t\t\t\t\t<tr><td style="min-width:250px;"><b>%s</b></td><td style="min-width:400px;">%s</td></tr>\n'%(config,value)
+                        html += IDENT_5 + '<tr><td style="min-width:300px;"><b>%s</b></td><td style="min-width:480px;">%s</td></tr>\n'%(config,value)
                         first=False
                     else:
-                        html += '\t\t\t\t\t<tr><td></td><td>%s</td></tr>\n'%(value)
+                        html += IDENT_5 + '<tr><td></td><td>%s</td></tr>\n'%(value)
 
-            html += '\t\t\t\t</table>\n'
+            html += IDENT_4 + '</table>\n'
             request_config_processed = True
         
         plugins = element["plugins"]
 
         
-        html += '\t\t\t\t<br><br>\n\t\t\t\t<table style="width:100%; background-color: lightskyblue; padding-left: 20px; border-collapse: separate; border-spacing: 1em; border: 1px solid black; border-radius: 10px;">\n\t\t\t\t\t<tr><td><h3>Results</h4></td></tr>\n'
-        html += '\t\t\t\t\t<tr><td style="min-width:250px;"><b>Target</b></td><td style="min-width:400px;">%s</td></tr>\n'%(target)
-        html += '\t\t\t\t\t<tr><td><b>HTTP Status</b></td><td>[ %s ]</td></tr>\n'%(http_status)
+        html += IDENT_4 + '<br><br>\n'
+        html += IDENT_4 + '<table style="width:100%; background-color: lightskyblue; padding-left: 20px; border-collapse: separate; border-spacing: 1em; border: 1px solid black; border-radius: 10px;">\n'
+        html += IDENT_5 + '<tr><td><h3>Results</h4></td></tr>\n'
+        html += IDENT_5 + '<tr><td style="min-width:300px;"><b>Target</b></td><td style="min-width:480px;"><b>%s</b></td></tr>\n'%(target)
+        html += IDENT_5 + '<tr><td><b>HTTP Status</b></td><td>[ %s ]</td></tr>\n'%(http_status)
 
         for plugin in plugins:
             if plugin == "Country": #special case where there is the 2 letter code
@@ -63,15 +68,18 @@ def create_html_report(jsonFile):
                 textValue = value[0]
             else:
                 textValue = ""
-            html += '\t\t\t\t\t<tr><td><b>%s</b></td><td>%s</td></tr>\n'%(plugin,textValue)
+            html += IDENT_5 + '<tr><td><b>%s</b></td><td>%s</td></tr>\n'%(plugin,textValue)
 
             for i in range(1,len(value)):
-                html += '\t\t\t\t\t<tr><td></td><td>%s</td></tr>\n'%(value[i])
+                html += IDENT_5 + '<tr><td></td><td>%s</td></tr>\n'%(value[i])
 
 
-        html += '\t\t\t\t</table>\n'
+        html += IDENT_4 + '</table>\n'
 
-    html += '\t\t\t</div>\n\t\t</div>\n\t</body>\n</html>'
+    html += """            </div>
+        </div>
+    </body>
+</html>"""
 
     htmlFilename = jsonFile.split(".json")[0]+".html"
     with open(htmlFilename, 'w', encoding='utf-8') as output_file:
@@ -84,4 +92,8 @@ if __name__ == "__main__":
 
     jsonFile = sys.argv[1]
 
-    create_html_report(jsonFile)
+    try:
+        create_html_report(jsonFile)
+    except Exception as e:
+        print("Failed to generate html from the json input file due to: " + str(e))
+        print("Please provide a valid json whatweb output file [ --log-json=json_file.json ]")
